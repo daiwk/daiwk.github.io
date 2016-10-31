5de7fcc6cf9a65d4d85344a129e3600102ff2f5b
daiwk <daiwk1989@gmail.com>
2016-11-01 02:35:58 +0800

    xx

---
+++ get_pre_embedding.sh
@@ -0,0 +1,6 @@
+for file in baidu.dict model_32.emb model_64.emb model_128.emb model_256.emb
+do 
+  # following is the google drive address
+    # you can also directly download from https://pan.baidu.com/s/1o8q577s
+      wget https://www.googledrive.com/host/0B7Q8d52jqeI9ejh6Q1RpMTFQT1k/embedding/$file --no-check-certificate
+done
