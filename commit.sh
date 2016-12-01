1d60943c3a4b316faf05c65ca25e1f0ecaf62f54
daiwk <daiwk1989@gmail.com>
2016-12-02 00:52:04 +0800

    xx

--- commit.sh
+++ commit.sh
@@ -1,4 +1,12 @@
+git config --global push.default matching
+git config --global push.default simple
+
 git add _posts
+git add _includes
+git add posts
+git add _config.yml
 git add assets
+git add index.html
+git add source_codes
 git commit -m "xx"
-git push origin
+git push --set-upstream origin master
